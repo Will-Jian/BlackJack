dc27d8ab97fe40d8350f9fd5b2eedf94d6e2817d
Will <willjian97@gmail.com>
2022-10-07 18:20:23 -0700

    created a deck function also added card spaces in the playerhand

--- js/main.js
+++ js/main.js
@@ -1,24 +1,27 @@
   /*----- constants -----*/
   //1.1) define the cards deck with 4 different suits and the cards within each suit.  (Spades,hearts,clover,diamonds)(Ace through king) 
   //Arr of obj 
-  const deck = [
-    {rank: 'heart', suit: "Ace" },
+  const deck = [ ]
+  const suits = [ "Spades","Diamonds","Club","Heart"]
+  const rank = [ "Ace",'2','3','4','5','6','7','8','9','10','Jack','Queen','King']
 
-  ]
-
-   // 1.2) define the board with 5 card positions on each side of the board with null spaces to identify they are empty before the game starts.  - cached
+  const COLORS = {
+    'null': 'white',
+    '1':'Yellow',
+    '-1' : 'Purple'
+  }
   
     // 1.3) define the winning conditions- if  player reaches 21 stop and wait for dealer or if the dealer reaches 21 stop and let dealer win. If both are 21 it is a tie. 
   
 
    // 1.4) How much wager they start out with  total amount of chips 
-  
+  const pot = 1000
 
   /*----- state variables -----*/
 let winner;
-let dealerBoard;
-let playerBoard;
-
+let dealerHand;
+let playerHand;
+let shuffledDeck; 
 
   /*----- cached elements */ 
 const messageEl = document.querySelector('h1')
@@ -30,16 +33,35 @@ startButton.addEventListener('click',start);
   init()
 
     function init(){
-      dealerBoard = [null,null,null,null,null]
-      playerBoard = [null,null,null,null,null]
+      dealerHand = [null,null,null,null,null]
+      playerHand = [null,null,null,null,null]
       winner = null
-      deckShuffle()
+      createDeck()
+      
       render()
 
   }
 
+  function createDeck(){
+    for (let i=0; i < suits.length; i++){     //iterate over ranks while iterating over the suits to create card
+      for (let j=0; j<rank.length; j++) {
+        let card = {Rank: rank[j], Suit: suits[i]};
+        deck.push(card);
+      }
+    }
+  }
+
+  function renderPlayerHand(){
+    playerHand.forEach(function(cellVal, cellidx){
+      console.log(cellVal,cellidx)
+      const playerhandEl =document.getElementById(`phand${cellidx}`)
+      playerhandEl.style.backgroundColor = COLORS[cellVal];
+    })
+  }
+
     function render(){
-    renderDeck()
+   renderPlayerHand()
+   renderDealerHand()
    
   }
 
