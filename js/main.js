0925762ceb51dcb632e71a84795010d43b554dfc
Will <willjian97@gmail.com>
2022-10-13 18:38:33 -0700

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-  /*----- constants -----*/
-  const originalDeck = [ ]
-  const suits = [ "s","d","c","h"]
-  const rank = [ '02','03','04','05','06','07','08','09','10','J','Q','K','A']
+
+const originalDeck = [ ];
+const suits = [ "s","d","c","h"];
+const rank = [ '02','03','04','05','06','07','08','09','10','J','Q','K','A']
+
   
-  
-
-
-
-  /*----- state variables -----*/
 let winner;
 let dealerHand;
 let playerHand;
@@ -15,61 +11,59 @@ let shuffledDeck;
 let betAmount;
 let pot;
 
-  /*----- cached elements */ 
-const startButton = document.getElementById('startbutton')
-const hitButton = document.getElementById('hitButton')
-const standButton = document.getElementById('standButton')
-const messageEl = document.getElementById('message')
-const betButton = document.getElementById('bet')
+const startButton = document.getElementById('startbutton');
+const hitButton = document.getElementById('hitButton');
+const standButton = document.getElementById('standButton');
+const messageEl = document.getElementById('message');
+const betButton = document.getElementById('bet');
 const wageamountEl = document.getElementById('wageamount');
-const wageBankEl = document.getElementById('wageBank')
+const wageBankEl = document.getElementById('wageBank');
 
-  /*----- event listeners -----*/
+
 startButton.addEventListener('click',start);
-hitButton.addEventListener('click',hitFunc)
-standButton.addEventListener('click',standFunc)
-betButton.addEventListener('click', betFunc)
-
-  /*----- functions -----*/
-  init()
+hitButton.addEventListener('click',hitFunc);
+standButton.addEventListener('click',standFunc);
+betButton.addEventListener('click', betFunc);
 
 
-  function init(){
+init()
+
+
+function init(){
     pot = 1000
-    startButton.style.visibility = "visible"
-    hitButton.style.visibility = "hidden" 
-    standButton.style.visibility = "hidden"  
+    startButton.style.visibility = "visible";
+    hitButton.style.visibility = "hidden"; 
+    standButton.style.visibility = "hidden"; 
 
     wageBankEl.innerHTML = pot
-      dealerHand = []
-      playerHand = []
-      playerPoints = 0
-      dealerPoints = 0
-      createDeck()
-      renderNewShuffledDeck() 
-  }
+      dealerHand = [];
+      playerHand = [];
+      playerPoints = 0;
+      dealerPoints = 0;
+      createDeck();
+      renderNewShuffledDeck();
+}
 
-  function createDeck(){
+function createDeck(){
     for (let i=0; i < suits.length; i++){     
       for (let j=0; j<rank.length; j++) {
-        let card = {face: `${suits[i]}${rank[j]}`, value: Number(rank[j]) || (rank[j] === 'A' ? 11 : 10), backside: "back-side"}
+        let card = {face: `${suits[i]}${rank[j]}`, value: Number(rank[j]) || (rank[j] === 'A' ? 11 : 10), backside: "back-side"};
         originalDeck.push(card);  
       }
     }
-  }
+}
 
-  function buildShuffledDeck(){
+function buildShuffledDeck(){
     const tempDeck = [...originalDeck];
     const newShuffledDeck = [];
     for (let i = 0; i <originalDeck.length; i++){
       const randomIdx = Math.floor(Math.random()*tempDeck.length);
-      newShuffledDeck.push(tempDeck.splice(randomIdx, 1)[0])
+      newShuffledDeck.push(tempDeck.splice(randomIdx, 1)[0]);
     }  
     return newShuffledDeck;
-  }
+}
 
-
-  function renderDeckInContainer(anyDeck,container){
+function renderDeckInContainer(anyDeck,container){
     container.innerHTML = '';
     let cardsHtml = '';
     let cardsHtml1 = '';
@@ -86,51 +80,44 @@ betButton.addEventListener('click', betFunc)
         cardsHtml1 += ` <div class="card ${anyDeck[i].face} faceup"></div>`; 
         cardsHtml1+= ` <div class="card-${anyDeck[i].backside}"></div>`;
         cardsHtml += `<div class = "flip-card"> ${cardsHtml1} </div>`;
-  }
+      }
         container.innerHTML += cardsHtml
     for (let i =1; i < anyDeck.length; i++){
         cardsHtml2 += `<div class="card ${anyDeck[i].face}"></div>`; 
     }
     container.innerHTML += cardsHtml2
 
-  }
-  }
+    }
+}
    
 
-  function renderNewShuffledDeck(){
+function renderNewShuffledDeck(){
     shuffledDeck = buildShuffledDeck();
-  }
-  function reshuffleDeck(){
+}
+function reshuffleDeck(){
     if (shuffledDeck = []){
           shuffledDeck = buildShuffledDeck()
     } else 
     return
-  }
-    
-  
+} 
 
-  function renderPlayerHand(){
+function renderPlayerHand(){
     renderDeckInContainer(playerHand, document.getElementById('playerHand'))
-  }
-  function renderDealerHand(){
+}
+function renderDealerHand(){
     renderDeckInContainer(dealerHand, document.getElementById('dealerHand'))
-  }
+}
 
- 
-
-
-
-  function render(){
+function render(){
     renderButtons();
     renderPlayerHand();
     renderDealerHand();
     renderMessage();
     reshuffleDeck();
     
-  }
+}
 
-
-  function start(){
+function start(){
     
     if (betAmount !== undefined && betAmount <= pot && betAmount !== 0){
       winner = null;
@@ -152,31 +139,19 @@ betButton.addEventListener('click', betFunc)
   } else {
       messageEl.innerHTML = "forgot to place bet!"
   }
-
 }
-/*
-  function hideHandIfWinner(){
-    if (winner === 'dealer' || winner === 'player'){
-      playerHand = []
-      dealerHand = []
-      betButton.style.visibility = "visible"
-    } else 
-      return
-    
-  }
 
-*/
 function checkHandScore(hand){
   let totalScore =0
   let totalAce =0
   hand.forEach(function addValue(obj) {
     if (obj.value == 11){totalAce += 1, totalScore+= obj.value} else {totalScore+= obj.value}
     })
-  while (totalScore > 21 && totalAce > 0) {
+    while (totalScore > 21 && totalAce > 0) {
     totalScore-=10
     totalAce-=1
-  }
-return totalScore;
+    }
+  return totalScore;
 }
 
 
@@ -184,7 +159,6 @@ function hitFunc(){
   playerHand.push(shuffledDeck.shift())
   playerPoints = checkHandScore(playerHand);
   winner = checkWinner();
-  //hideHandIfWinner()
   render()
 }
 
@@ -201,10 +175,8 @@ function standFunc(){
         else {
         winner = checkWinner()
       }
-      //hideHandIfWinner()
-     
       render()
-  }
+}
   
 
 
@@ -264,22 +236,19 @@ function betFunc (){
     messageEl.innerHTML = "Not enough"
   } else if (wageamountEl.value === ""){
     messageEl.innerHTML = "place bet"
-  }
-  
-  else {
+  } else {
   messageEl.innerHTML = ""
-  
   }
-  }
+}
 
 function renderMessage(){
-    if (winner === "player") {
-      messageEl.innerHTML = "You Won"
-    } else if ( winner === "dealer"){
+  if (winner === "player") {
+    messageEl.innerHTML = "You Won"
+  } else if ( winner === "dealer"){
       messageEl.innerHTML = "Dealer Won"
-    }  else {
-      messageEl.innerHTML = ""
-    }
+  } else {
+    messageEl.innerHTML = ""
+  }
 }
 
 function renderButtons() {
